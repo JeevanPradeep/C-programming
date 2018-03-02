c6639655b77a69e6cfedbfc4071469098ca17b7f
JeevanPradeep <34646153+JeevanPradeep@users.noreply.github.com>
2018-03-02 12:35:39 +0530

    Odd numbers between two intervals

---
+++ set1/14.c
@@ -0,0 +1,14 @@
+#include<stdio.h>  
+int main()
+{
+    int s,l;
+    scanf("%d",&s);
+    scanf("%d",&l);
+    for(int i=s+1;i<=l-1;i++)
+    {
+        if(i%2!=0)
+        {
+            printf("%d ",i);
+        }
+    }
+}
